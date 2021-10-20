a2802dbbff16259eda60d41be1a3cc1fd1771ec4
Your Name <since@qq.com>
2021-10-20 18:45:27 +0800

    gitci修改

--- ci/deploy_dev01.sh
+++ ci/deploy_dev01.sh
@@ -4,4 +4,4 @@ tar zxvf ./../frontend.tar.gz
 cd ..
 docker build -f ./ci/Dockerfile -t $IMAGE_REPO_PREFIX-dev01:$IMAGE_VER .
 docker stop $CONTAINER_NAME_PREFIX-dev01 || true && docker rm $CONTAINER_NAME_PREFIX-dev01 || true
-docker run -d -p 127.0.0.1:21365:80 --name $CONTAINER_NAME_PREFIX-dev01 $IMAGE_REPO_PREFIX-dev01:$IMAGE_VER
+docker run -d -p 0.0.0.0:21365:80 --name $CONTAINER_NAME_PREFIX-dev01 $IMAGE_REPO_PREFIX-dev01:$IMAGE_VER
--- ci/deploy_master.sh
+++ ci/deploy_master.sh
@@ -4,4 +4,4 @@ tar zxvf ./../frontend.tar.gz
 cd ..
 docker build -f ./ci/Dockerfile -t $IMAGE_REPO_PREFIX-master:$IMAGE_VER .
 docker stop $CONTAINER_NAME_PREFIX-master || true && docker rm $CONTAINER_NAME_PREFIX-master || true
-docker run -d -p 127.0.0.1:21366:80 --name $CONTAINER_NAME_PREFIX-master $IMAGE_REPO_PREFIX-master:$IMAGE_VER
+docker run -d -p 0.0.0.0:21366:80 --name $CONTAINER_NAME_PREFIX-master $IMAGE_REPO_PREFIX-master:$IMAGE_VER
