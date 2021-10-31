c24c1a738dc5983edceea37e12f5bd5dac210f48
Sincebook <40172250+Sincebook@users.noreply.github.com>
2021-10-31 20:31:57 +0800

    微信支付初始化

---
+++ src/utils/wxpay.js
@@ -0,0 +1,38 @@
+function onBridgeReady(appId, timeStamp, nonceStr, wx_package, paySign) {
+  window.WeixinJSBridge.invoke(
+    'getBrandWCPayRequest', {
+      'appId': appId, // 公众号名称，由商户传入
+      'timeStamp': timeStamp, // 时间戳，自1970年以来的秒数
+      'nonceStr': nonceStr, // 随机串
+      'package': wx_package,
+      'signType': 'MD5', // 微信签名方式：
+      'paySign': paySign // 微信签名
+    },
+    function (res) {
+      if (res.err_msg == 'get_brand_wcpay_request:ok' ) {
+        console.log('pay ok');
+        // 使用以上方式判断前端返回,微信团队郑重提示：
+        // res.err_msg将在用户支付成功后返回ok，但并不保证它绝对可靠。
+        // mui.alert('支付成功', function () {
+        //   toIndex();
+        // });
+      }
+    });
+}
+
+function final(appId, timeStamp, nonceStr, wx_package, paySign) {
+  if (typeof WeixinJSBridge == 'undefined') {
+    if (document.addEventListener) {
+      console.log('document.addEventListener')
+      document.addEventListener('WeixinJSBridgeReady', onBridgeReady, false);
+    } else if (document.attachEvent) {
+      console.log('document.attachEvent')
+      document.attachEvent('WeixinJSBridgeReady', onBridgeReady);
+      document.attachEvent('onWeixinJSBridgeReady', onBridgeReady);
+    }
+  } else {
+    onBridgeReady(appId, timeStamp, nonceStr, wx_package, paySign);
+  }
+}
+
+export { final, onBridgeReady }
