a790f72ee700a75fd76616956f53346c5bc635b7
since <1336594801@qq.com>
2021-12-23 22:57:33 +0800

    popmart奖池

--- src/views/Fashions/popmart.vue
+++ src/views/Fashions/popmart.vue
@@ -6,6 +6,10 @@
       left-arrow
       @click-left="onClickLeft"
     />
+    <img src="../../assets/popmart/1.png" style="width:100%">
+    <van-cell title="密森古堡系列" value="市场零售价69元" value-class="price"/>
+    <img src="../../assets/popmart/2.png" style="width:100%">
+    <van-cell title="密森古堡系列" value="市场零售价69元" value-class="price"/>
   </div>
 </template>
 
@@ -20,5 +24,8 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
+  .price {
+    color:rgb(218, 27, 27);
+  }
 </style>
